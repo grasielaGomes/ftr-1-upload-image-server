cac7a1245ae7f16dca3f03c666941e766d8008e0
grasielaGomes <produtoradama@gmail.com>
2025-01-20 19:34:42 -0300

    feat: Variáveis ambiente com Zod

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,11 +1,14 @@
-import Fastify from 'fastify'
+import { env } from '@/env'
 import fastifyCors from '@fastify/cors'
+import Fastify from 'fastify'
 
 const server = Fastify()
 
 server.register(fastifyCors, {
   origin: '*',
 })
+
+console.log(env.DATABASE_URL)
 
 server.listen({ port: 3333, host: '0.0.0.0' }, (err, address) => {
   if (err) {
